d34b524d368ce5257f96bcbcce3224826174a5a4
Dmitry Drak <dmitry8drak@gmail.com>
2021-12-11 03:07:25 +0300

    Added get_picture_of call in bot.py, added new exception handling

--- bot.py
+++ bot.py
@@ -7,14 +7,15 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши название лекарства")
-    if '!' in message.text[-1]:
+        bot.send_message(message.from_user.id, 
+        """Напиши название лекарства (действующее вещество)
+        с восклицательным знаком в конце. Регистр не важен.\nНапример - Клозапин!!""")
+
+    else:
         drug_name = message.text.replace('!', '')
         try:
-            bot.send_photo(message.from_user.id, return_chemical_form_of(drug_name))
-        except IndexError:
-            bot.send_message(message.from_user.id, "Такое лекарство не найдено! Попробуйте снова.")
-    else:
-        bot.send_message(message.from_user.id, "Напиши /help.")
+            bot.send_photo(message.from_user.id, get_picture_of(drug_name))
+        except KeyError or IndexError:
+            bot.send_message(message.from_user.id, "Такое лекарство не найдено! Возможно Вы ввели торговое название (Найз - торговое название, нимесулид - вещество)\nПопробуйте снова.")
 
 bot.polling(none_stop=True, interval=0)
